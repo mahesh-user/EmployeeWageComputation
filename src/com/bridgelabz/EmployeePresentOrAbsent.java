6d5ae03de42b300bca90035fde092e6344961822
Mahesh <mahesh51138@gmail.com>
2022-01-20 12:55:56 +0530

    Added : CalcuLATION OF EMPLOYEE WAGE

--- src/com/bridgelabz/EmployeePresentOrAbsent.java
+++ src/com/bridgelabz/EmployeePresentOrAbsent.java
@@ -3,16 +3,24 @@ package com.bridgelabz;
 import java.util.Random;
 
 public class EmployeePresentOrAbsent {
+
     public static void main(String[] args) {
         Random random = new Random();
+        int present = 1;
+        int wage = 20;
+        int hour = 8;
+        int totalWage;
         int check = random.nextInt(2);
-        if (check == 1){
+        if (check == present){
             System.out.println("Employee is Present");
+            totalWage = wage * hour;
         }
         else{
             System.out.println("Employee is Absent");
+            totalWage = 0;
         }
 
+        System.out.println("Total wage of employee :" + totalWage);
 
     }
 }
