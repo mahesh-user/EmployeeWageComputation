80baeca85dba53e7896146e7e9008438f46a08a9
Mahesh <mahesh51138@gmail.com>
2022-01-20 12:42:59 +0530

    Added : Employee present or absent

--- src/com/bridgelabz/EmployeePresentOrAbsent.java
+++ src/com/bridgelabz/EmployeePresentOrAbsent.java
@@ -1,7 +1,18 @@
 package com.bridgelabz;
 
+import java.util.Random;
+
 public class EmployeePresentOrAbsent {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Employee Wage Computation");
+        Random random = new Random();
+        int check = random.nextInt(2);
+        if (check == 1){
+            System.out.println("Employee is Present");
+        }
+        else{
+            System.out.println("Employee is Absent");
+        }
+
+
     }
 }
