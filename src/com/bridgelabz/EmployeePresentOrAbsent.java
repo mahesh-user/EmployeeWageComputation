00ea72b23398dda01dd1ec78e2df14394b10b0c3
Mahesh <mahesh51138@gmail.com>
2022-01-20 22:44:17 +0530

    added:monthly Wage

--- src/com/bridgelabz/EmployeePresentOrAbsent.java
+++ src/com/bridgelabz/EmployeePresentOrAbsent.java
@@ -11,25 +11,34 @@ public class EmployeePresentOrAbsent {
         int wagePerHour = 20;
         int partTime = 4;
         int fullTime = 8;
-        int totalWage;
-        int check = random.nextInt(3);
-        switch (check ) {
-            case present:{
-               System.out.println("Employee is present");
-               totalWage = wagePerHour * fullTime;
-               break;
+        int dailyWage = 0;
+        int totalWage = 0;
+        for(int i=0; i<20; i++) {
+
+            int check = random.nextInt(3);
+
+            switch (check) {
+                case present: {
+                    System.out.println("Employee is present");
+                    dailyWage = wagePerHour * fullTime;
+                    totalWage = dailyWage + totalWage;
+                    break;
+                }
+                case absent: {
+                    System.out.println("Employee is absent");
+                    dailyWage = 0;
+                    totalWage = dailyWage + totalWage;
+                    break;
+                }
+                default: {
+                    System.out.println("Employee is part time");
+                    dailyWage = wagePerHour * partTime;
+                    totalWage = dailyWage + totalWage;
+                    break;
+                }
             }
-            case absent:{
-                System.out.println("Employee is absent");
-                totalWage = 0;
-                break;
-            }
-            default:{
-                System.out.println("Employee is part time");
-                totalWage =wagePerHour*partTime;
-                break;
-            }
-            }
-        System.out.println("Total Wage : " + totalWage);
+        }
+        System.out.println
+                ("Total Wage : " + totalWage);
         }
 }
